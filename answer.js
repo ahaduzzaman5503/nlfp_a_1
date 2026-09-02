246d459857805434373918e578f9a98c9eb48d5b
unknown <ahaduzzaman5503@gmail.com>
2026-09-02 22:08:39 +0600

    Problem 4 solved

--- answer.js
+++ answer.js
@@ -43,4 +43,25 @@ function validateUsername (username) {
 
 
 // Question No. 4
+
+function getCngFare(distance, isNight = false, waitingMinutes = 0) {
+    let fare = 50;
+    if (distance > 2) {
+        fare += (distance - 2) * 15;
+    }
+        fare += waitingMinutes * 2; 
+    if (isNight) {
+        fare *= 1.2; 
+    }
+    return fare;
+}
+
+
+
+
+
+
+
+
+
 // Question No. 5
